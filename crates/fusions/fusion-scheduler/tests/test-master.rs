126a337bb534c6635abe6e7cefdd2a642d3adfab
Yang Jing <yang.xunjing@qq.com>
2024-12-13 16:19:39 +0800

    chore: Move documents

--- crates/fusions/fusion-scheduler/tests/test-master.rs
+++ crates/fusions/fusion-scheduler/tests/test-master.rs
@@ -1,12 +1,21 @@
+use std::{
+  process::{exit, ExitCode},
+  time::Duration,
+};
+
 use fusion_scheduler::broker::loop_master;
-use ultimate::application::Application;
+use ultimate::{application::Application, timer::TimerPlugin};
 use ultimate_db::DbPlugin;
 
 #[tokio::test]
 async fn test_master_init() {
-  Application::builder().add_plugin(DbPlugin).run().await.unwrap();
+  Application::builder().add_plugin(TimerPlugin).add_plugin(DbPlugin).run().await.unwrap();
   let app = Application::global();
 
   // let (db_tx, db_rx) = mpsc::channel(1024);
-  loop_master(app.clone()).await.unwrap()
+  tokio::spawn(loop_master(app.clone()));
+
+  tokio::time::sleep(Duration::from_secs(2)).await;
+  println!("Sleeped 10 seconds, exiting...");
+  exit(0);
 }
